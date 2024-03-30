68f95f7bd226fc5951ec6f143a1e1e913dc1d346
sylvaindev <sylvaindelange@hotmail.com>
2024-03-30 13:27:33 +0100

    toevoegen

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
-
+# hieronder bepaalt uw de taal van uw applicatie
 LANGUAGE_CODE = 'nl-nl'
 
 TIME_ZONE = 'UTC'
